e5206ede669779b8a580db8bebeb883a65639bc7
lununglee <leo.lunung.lee@gmail.com>
2020-03-17 13:14:43 -0700

    added message on terminal screen

---
+++ Assets/Hacker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+	// Start is called before the first frame update
+	void Start()
+	{
+		Terminal.WriteLine("Booting...")
+		Menu();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+
+	void	Menu()
+	{
+		Terminal.WriteLine("Choose infrastructure to hack\n");
+		Terminal.WriteLine("Press 1 for local library");
+		Terminal.WriteLine("Press 2 for police station");
+		Terminal.WriteLine("Press 3 for NASA\n");
+		Terminal.WriteLine("Enter your selection:");
+	}
+}
