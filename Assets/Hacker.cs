b285be049f042c402d0ac709759eab0f1ab7787d
lununglee <leo.lunung.lee@gmail.com>
2020-03-17 16:35:26 -0700

    implemented layered input processing through game state

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -7,7 +7,8 @@ public class Hacker : MonoBehaviour
 	// Game State
 	int		Level = 0;
 	enum	Screen { MainMenu, Password, Win };
-	Screen	CurrentScreen = Screen.MainMenu;
+	Screen	CurrentScreen;
+	string	Password;
 
 	// Start is called before the first frame update
 	void Start()
@@ -25,15 +26,26 @@ public class Hacker : MonoBehaviour
 		Terminal.WriteLine("Enter your selection:");
 	}
 
+	void	PrintSecretMenu()
+	{
+		Terminal.ClearScreen();
+		Terminal.WriteLine("No OS detected...");
+	}
+
 	void	OnUserInput(string Input)
 	{
 		if (Input.ToLower() == "menu")
 		{
+			CurrentScreen = Screen.MainMenu;
 			PrintMenu();
 		}
 		else if (CurrentScreen == Screen.MainMenu)
 		{
 			RunMainMenu(Input);
+		}
+		else if (CurrentScreen == Screen.Password)
+		{
+			CheckPassword(Input);
 		}
 	}
 
@@ -42,25 +54,41 @@ public class Hacker : MonoBehaviour
 		if (Input == "1")
 		{
 			Level = 1;
+			Password = "one";
 			StartGame();
 		}
 		else if (Input == "2")
 		{
 			Level = 2;
+			Password = "two";
 			StartGame();
 		}
 		else if (Input == "3")
 		{
 			Level = 3;
+			Password = "three";
 			StartGame();
 		}
 		else if (Input == "rm -rf /")
 		{
-			Terminal.WriteLine("Are you sure you want to delete the database?");
+			Terminal.WriteLine("Deleting database...");
+			PrintSecretMenu();
 		}
 		else
 		{
 			Terminal.WriteLine("Please enter a valid option");
+		}
+	}
+
+	void	CheckPassword(string Input)
+	{
+		if (Input == Password)
+		{
+			Terminal.WriteLine("Authentication Successful...");
+		}
+		else
+		{
+			Terminal.WriteLine("Incorrect password, please retry...");
 		}
 	}
 
