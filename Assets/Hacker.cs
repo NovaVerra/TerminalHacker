3e85c465c84d13ea815565dd684dfd11d8019024
lununglee <leo.lunung.lee@gmail.com>
2020-03-17 15:55:59 -0700

    changed passing argument to holding game state

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
+	// Game State
+	int	Level = 0;
+
 	// Start is called before the first frame update
 	void Start()
 	{
-		string Greeting = "Hello Leo";
-		PrintMenu(Greeting);
+		PrintMenu();
 	}
-	
-	void	PrintMenu(string Greeting)
+
+	void	PrintMenu()
 	{
 		Terminal.ClearScreen();
-		Terminal.WriteLine(Greeting);
 		Terminal.WriteLine("Choose infrastructure to hack\n");
 		Terminal.WriteLine("Press 1 for local library");
 		Terminal.WriteLine("Press 2 for police station");
@@ -24,7 +25,37 @@ public class Hacker : MonoBehaviour
 
 	void	OnUserInput(string Input)
 	{
-		// Terminal.WriteLine("The user typed " + Input);
-		print(Input == "1");
+		if (Input == "1")
+		{
+			Level = 1;
+			StartGame();
+		}
+		else if (Input == "2")
+		{
+			Level = 2;
+			StartGame();
+		}
+		else if (Input == "3")
+		{
+			Level = 3;
+			StartGame();
+		}
+		else if (Input.ToLower() == "menu")
+		{
+			PrintMenu();
+		}
+		else if (Input == "rm -rf /")
+		{
+			Terminal.WriteLine("Are you sure you want to delete the database?");
+		}
+		else
+		{
+			Terminal.WriteLine("Please enter a valid option");
+		}
+	}
+
+	void	StartGame()
+	{
+		Terminal.WriteLine("You've chosen level: " + Level);
 	}
 }
